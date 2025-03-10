4add44fb05dbc38e9cb556b2dbd34206d011e9ae
rsabbah <rpasabb@gmail.com>
2025-03-10 18:23:49 +0100

    Host resolution

--- inc/ft_ping.h
+++ inc/ft_ping.h
@@ -2,5 +2,25 @@
 #define FT_PING
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <err.h>
+#include <errno.h>
+#include <string.h>
+
+/* Error */
+#define ERR_NO_HOST	"missing host operand"
+#define ERR_UNK_HOST	"unknown host"
+
+struct ft_ping {
+	int sockfd;
+	struct sockaddr dst;
+};
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,58 @@
 #include "../inc/ft_ping.h"
 
+int create_socket() {
+	int sockfd;
+
+	sockfd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (sockfd == -1) {
+		errx(EXIT_FAILURE, "%s", strerror(errno));
+	}
+	return sockfd;
+}
+
+struct sockaddr resolve_hostanme(const char *hostname) {
+	struct addrinfo *res, hint;
+	struct sockaddr dst;
+	int ret;
+
+	res = NULL;
+	memset(&hint, 0, sizeof(struct addrinfo));
+	hint.ai_family = AF_INET;
+	hint.ai_socktype = SOCK_RAW;
+	hint.ai_protocol = IPPROTO_ICMP;
+	ret = getaddrinfo(hostname, NULL, &hint, &res);
+	if (ret == -1) {
+		errx(EXIT_FAILURE, "%s", gai_strerror(errno));
+	}
+	if (!res) {
+		errx(EXIT_FAILURE, "%s", ERR_UNK_HOST);
+	}
+	if (memcpy(&dst, res->ai_addr, sizeof(struct sockaddr)) == NULL) {
+		errx(EXIT_FAILURE, "%s", strerror(errno));
+	}
+	freeaddrinfo(res);
+	return dst;
+}
+
+void clean_all(int status, void *data) {
+	struct ft_ping *ft_ping;
+
+	(void)status;
+	ft_ping = (struct ft_ping*)data;
+	if (ft_ping->sockfd) {
+		close(ft_ping->sockfd);
+	}
+}
+
 int main(const int argc, const char **argv) {
-	(void)argc;
-	(void)argv;
+	struct ft_ping ft_ping;
+
+	if (argc != 2) {
+		errx(EXIT_FAILURE, "%s", ERR_NO_HOST);
+	}
+	memset(&ft_ping, 0, sizeof(struct ft_ping));
+	on_exit(clean_all, &ft_ping);
+	ft_ping.sockfd = create_socket();
+	ft_ping.dst = resolve_hostanme(argv[1]);
 	return EXIT_SUCCESS;
 }
