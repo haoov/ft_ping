3dff3c49f099bc6f8807242bfb498cc61f101b00
rsabbah <rpasabb@gmail.com>
2025-12-05 16:54:09 +0100

    [#5:ADD] ping_host function to handel ping per host with interrupt handling

--- src/ping.c
+++ src/ping.c
@@ -1,7 +1,26 @@
 #include "../inc/ft_ping.h"
 
-void ping_host(char *host, struct ping *p) {
-	// TODO: ping single host with interrupt handling for host 0  
+extern sig_atomic_t interrupt;
+
+void ping_host(struct ping *p) {
+	int count = get_opt(p->opts, "count", 0)->val.intgr;
+	double interval = get_opt(p->opts, "interval", 0)->val.dbl;
+
+	while (true) {
+
+		if (interrupt && p->stats.seq > 1) {
+			return;
+		}
+		icmp_echo_request(p);
+		icmp_response(p);
+		++p->stats.seq;
+		if (count != 0 && (int)p->stats.nsend >= count) {
+			return;
+		}
+		if (!interrupt) {
+			usleep(interval * 1000000);
+		}
+	}
 }
 
 void ft_ping(struct ping *p) {
@@ -17,18 +36,7 @@ void ft_ping(struct ping *p) {
 			get_opt(p->opts, "size", 0)->val.intgr
 		);
 
-		double interval = get_opt(p->opts, "interval", 0)->val.dbl;
-		while (true) {
-			int count = get_opt(p->opts, "count", 0)->val.intgr;
-
-			icmp_echo_request(p);
-			icmp_response(p);
-			++p->stats.seq;
-			if (count != 0 && (int)p->stats.nsend >= count) {
-				break;
-			}
-			usleep(interval * 1000000);
-		}
+		ping_host(p);
 
 		ping_stats(host, p->stats);
 	}
